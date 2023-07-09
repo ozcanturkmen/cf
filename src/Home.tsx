fe5ee2633b11281b9f16694f66b95f28221c45b2
Ozcan Turkmen <subatkuzgunu@gmail.com>
2023-07-09 16:22:27 +0300

    Add _routes.json

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <h1>React 18 Tutorial</h1>
+      <h1>React 18</h1>
       <p>
         <Link to="/lesson1">Your First Componentzz</Link>
       </p>
